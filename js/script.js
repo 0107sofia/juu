0348db75df4b09479a02be197bb5e6b89884c852
marcus <marcusssu@gmail.com>
2015-06-18 18:04:06 -0700

    account number fixed

--- js/script.js
+++ js/script.js
@@ -2,13 +2,28 @@ $(document).ready(function () {
 $('.account').click(function () {
     //Conditional states allow the dropdown box appear and disappear 
     if ($('#signin-dropdown').is(":visible")) {
-        $('#signin-dropdown').hide()
-    $('#session').removeClass('active'); // When the dropdown is not visible removes the class "active"
+        $('#account img').attr('src','img/account-gray.png');
+
+
+        $('#signin-dropdown').hide();
+        $('#session').removeClass('active'); // When the dropdown is not visible removes the class "active"
     } else {
-        $('#signin-dropdown').show()
-    $('#session').addClass('active'); // When the dropdown is visible add class "active"
+        $('#signin-dropdown').show();
+        $('#account img').attr('src','img/account-blue.png');
+        $('#session').addClass('active'); // When the dropdown is visible add class "active"
     }
     return false;
+});
+
+$( ".account" ).mouseover(function() {
+  $('#account img').attr('src','img/account-blue.png');
+});
+$( ".account" ).mouseout(function() {
+    if ($('#signin-dropdown').is(":visible")) {
+
+    }else{
+  $('#account img').attr('src','img/account-gray.png');
+}
 });
 // Allow to hide the dropdown box if you click anywhere on the document.
 $('#signin-dropdown').click(function(e) {
@@ -16,6 +31,7 @@ e.stopPropagation();
 });
 $(document).click(function() {
     $('#signin-dropdown').hide();
+    $('#account img').attr('src','img/account-gray.png');
 $('#session').removeClass('active');
 });
 });   
