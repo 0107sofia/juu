f98e4ca2ed48e375a23996f6a9726ff776897479
marcusssu <marcusssu@gmail.com>
2015-07-01 22:47:12 -0700

    auto loading done, still have bugs

--- js/masonry.js
+++ js/masonry.js
@@ -4,34 +4,44 @@
 //     itemSelector: '.product'
 //    // "gutter": 5
 
-    
-//   });
   
+//   });
+
 // });
 
 
 // external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
 $(document).ready( function() {
-  // init Masonry
-  var $grid = $('.masonry-container').masonry({
-    itemSelector: '.product',
-    percentPosition: true,
-    columnWidth: '.product-sizer'
-  });
+// init Masonry
+var $grid = $('.masonry-container').masonry({
+  itemSelector: '.product',
+  percentPosition: true,
+  columnWidth: '.product'
+});
 
-  $(window).scroll(function() {
-    //if user scroll to the bot
-    var productscount = $(".masonry-container .product").length;
-    var simple = 0;
-    simple = $('.simple .product').clone();
-    if($(window).scrollTop() == $(document).height() - $(window).height()) {
-           console.log(productscount);
-           console.log(simple);
-           
-           $grid.append( simple ).masonry( 'appended', simple );
-    }
-  });
+
+    $grid.masonry();
+  
+
+var imagecounter=20;
+$(window).scroll(function() {
+  //if user scroll to the bot
+  var productscount = $(".masonry-container .product").length;
+  
+  var $sample = $('.sample .product').clone();
+
+  
+  if($(window).scrollTop() == $(document).height() - $(window).height()&imagecounter<38) {
+
+      $sample.find('img').attr('src','img/productlist/all/product'+imagecounter+'.jpg');
+      $grid.append( $sample ).masonry('appended',$sample);
+      $grid.masonry();
+      imagecounter++;
+
+  }
+  $grid.masonry();
+});
 
 
 });
